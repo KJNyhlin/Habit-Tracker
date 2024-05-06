46735f19c8465b0986a593b5b5024b9a5c233572
KJNyhlin <karlnyhlin@me.com>
2024-05-06 10:21:28 +0200

    HabitListView

--- HabitMaker/ContentView.swift
+++ HabitMaker/ContentView.swift
@@ -10,23 +10,11 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+            VStack {
+                HabitListView()
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -38,34 +26,22 @@ struct ContentView: View {
                     }
                 }
             }
-            Text("Select an item")
         }
+        
+        Text("Select an item")
     }
-
+    
+    
     private func addItem() {
         withAnimation {
+            // Varje cell ska visa namnet på vanan och streak (OBS alltså inte datumet då det skapades) samt en knapp för "done". Man kan också klicka på texten för att komma till en sida som visar komplett info om den vanan (Infosida).
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
             newItem.streak = 0
             newItem.done = false
             //TODO: här ska man få ange namn
-            newItem.name = "new habit"
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            newItem.name = "another habit"
+            
             do {
                 try viewContext.save()
             } catch {
@@ -77,6 +53,8 @@ struct ContentView: View {
         }
     }
 }
+
+
 
 private let itemFormatter: DateFormatter = {
     let formatter = DateFormatter()
---
+++ HabitMaker/HabitListView.swift
@@ -0,0 +1,68 @@
+//
+//  HabitListView.swift
+//  HabitMaker
+//
+//  Created by Karl Nyhlin on 2024-05-03.
+//
+
+import SwiftUI
+
+struct HabitListView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        //predicate: NSPredicate(format: "done == %d", false), // om man vill visa ett urval
+        animation: .default)
+    private var items: FetchedResults<Item>
+    var body: some View {
+        List {
+            ForEach(items) { item in
+                //NavigationLink {
+                //    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                HStack{
+                    if let name = item.name {
+                        Text(name)
+                    }
+                    Spacer()
+                    //if item.streak > 0 { // uncomment before release
+                    Text("Streak: " + String(item.streak))
+                    Spacer()
+                    //} // uncomment before release
+                    
+                    Button(action: {}) {
+                        Image(systemName: item.done ? "checkmark.square" : "square")
+                    }
+                    
+                    
+                    //label: {
+                    //Text(item.timestamp!, formatter: itemFormatter)
+                }
+            }
+            .onDelete(perform: deleteItems)
+            
+        }
+        
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    struct HabitListView_Previews: PreviewProvider {
+        static var previews: some View {
+            HabitListView()
+        }
+    }
+}
